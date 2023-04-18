ad348b89678a75ad11abb760ea9fd6ccf6ce0d3c
ttomn <tomn@cs.huji.ac.il>
2023-04-18 13:06:22 +0300

    Full pipeline

--- pre-process/add_streets_pipleine.py
+++ pre-process/add_streets_pipleine.py
@@ -4,6 +4,13 @@ import geopandas as gpd
 import folium
 from shapely.geometry import Point
 
+DIR_PATH = "C:\\Users\\ttomn\\OneDrive\\Desktop\\data-science-transportation-research"
+STREET_FILE_NAME = "NYC Street Centerline (CSCL).geojson"
+FILES_NAMES_TO_EXECUTE = ["Motor_Vehicle_Collisions_-_Crashes.csv",
+                          "NYPD_B_Summons__Year_to_Date_.csv",
+                          "NYPD_B_Summons__Historic_.csv"]
+IS_GEO_FILES = False
+
 
 def split_file(file_path: str, frag_amount: int):
     df = pd.read_csv(file_path)
@@ -13,9 +20,14 @@ def split_file(file_path: str, frag_amount: int):
         frag_df.to_csv(f"{file_path[:-4]}{i}{file_path[-4:]}")
 
 
-def add_columns_non_geo(df, streets_df, streets_buffered_df):
+def add_columns_non_geo(streets_df, streets_buffered_df, file_name):
+    df = pd.read_csv(f"{DIR_PATH}\\{file_name}")
+    longitude, latitude = get_long_lat_names(df)
+    df_not_na = df[(df[longitude].notna()) & (df[latitude].notna())]
+    print(f"data had {df.shape[0]} rows and {df_not_na.shape[0]} have valid non-na location")
+
     def get_closest_street(sample):
-        point = Point(sample[["LONGITUDE", "LATITUDE"]])
+        point = Point(sample[[longitude, latitude]])
         df_streets_filtered = streets_df[streets_buffered_df.contains(point)]
         if df_streets_filtered.shape[0] == 0:
             street_index = streets_df.distance(point).argmin()
@@ -24,33 +36,44 @@ def add_columns_non_geo(df, streets_df, streets_buffered_df):
             street_index = df_streets_filtered.iloc[street_filtered_index]["index"]
         return streets_df.iloc[street_index]["geometry"], street_index
 
-    df[['STREET', 'ST_INDEX']] = df.apply(get_closest_street, axis=1, result_type='expand')
-    return df
+    df_not_na[['STREET', 'ST_INDEX']] = df_not_na.apply(get_closest_street, axis=1, result_type='expand')
+    df_not_na.to_csv(f"{DIR_PATH}\\with_streets_not_na_{file_name}")
+    # plot_with_street_non_geo(df_not_na)
+
+
+def get_long_lat_names(df):
+    if "latitude" in df.columns:
+        return "longitude", "latitude"
+    if "Latitude" in df.columns:
+        return "Longitude", "Latitude"
+    if "LATITUDE" in df.columns:
+        return "LONGITUDE", "LATITUDE"
+    raise Exception
+
+
+def add_columns_geo(streets_df, streets_buffered_df, file_name):
+    pass
 
 
 def plot_with_street_non_geo(df):
+    longitude, latitude = get_long_lat_names(df)
     m = folium.Map(location=[40.7831, -73.9712], zoom_start=12)
     for i in range(100):
-        col1 = Point(df[["LONGITUDE", "LATITUDE"]].iloc[i:i + 1, ])
+        col1 = Point(df[[longitude, latitude]].iloc[i:i + 1, ])
         folium.GeoJson(df["STREET"].iloc[i]).add_to(m)
         folium.GeoJson(col1).add_to(m)
     m.save('outcome.html')
 
 
 def main():
-    collisions_file_path = "C:\\Users\\ttomn\\OneDrive\\Desktop\\data-science-transportation-research" \
-                           "\\Motor_Vehicle_Collisions_-_Crashes.csv"
-    streets_file_path = "C:\\Users\\ttomn\\OneDrive\\Desktop\\data-science-transportation-research" \
-                        "\\NYC Street Centerline (CSCL).geojson"
-    collisions_df = pd.read_csv(collisions_file_path)
-    streets_df = gpd.read_file(streets_file_path)
-    collisions_df_not_na = collisions_df[
-        (collisions_df["LONGITUDE"].notna()) & (collisions_df["LATITUDE"].notna())]
-    small_collisions = collisions_df_not_na.iloc[:100]
+    streets_df = gpd.read_file(f"{DIR_PATH}\\{STREET_FILE_NAME}")
     streets_df = streets_df.reset_index()
     streets_buffered_df = streets_df.buffer(0.00015)
-    small_collisions = add_columns_non_geo(small_collisions, streets_df, streets_buffered_df)
-    plot_with_street_non_geo(small_collisions)
+    for file_name in FILES_NAMES_TO_EXECUTE:
+        if IS_GEO_FILES:
+            add_columns_geo(streets_df, streets_buffered_df, file_name)
+        else:
+            add_columns_non_geo(streets_df, streets_buffered_df, file_name)
 
 
 if __name__ == '__main__':

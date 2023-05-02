153dd0075c4a6597b98b3f21482a056d6c5d3ad6
ttomn <tomn@cs.huji.ac.il>
2023-05-02 16:53:54 +0300

    Add more function for aggregation and cleaning

--- pre-process/add_streets_pipleine.py
+++ pre-process/add_streets_pipleine.py
@@ -17,12 +17,12 @@ STREET_FILE_NAME = "NYC Street Centerline (CSCL).geojson"
 #                          "NYPD_B_Summons__Year_to_Date_.csv",
 #                          "NYPD_B_Summons__Historic_.csv"]
 
-GEO_FILES_NAMES_TO_EXECUTE = ["VZV_Speed Humps.geojson",
-                              "VZV_Speed Limits.geojson",
-                              "VZV_Street Improvement Projects (SIPs) intersections.geojson",
-                              "VZV_Turn Traffic Calming.geojson",
-                              "VZV_Leading Pedestrian Interval Signals.geojson",
-                              "Points Of Interest.geojson"]
+GEO_FILES_NAMES_TO_EXECUTE = ["Points Of Interest.geojson"]
+                              # "VZV_Speed Humps.geojson",
+                              # "VZV_Speed Limits.geojson",
+                              # "VZV_Street Improvement Projects (SIPs) intersections.geojson",
+                              # "VZV_Turn Traffic Calming.geojson",
+                              # "VZV_Leading Pedestrian Interval Signals.geojson",
 
 IS_GEO_FILES = True
 IS_NON_GEO_FILES = False
@@ -101,6 +101,35 @@ def add_columns_geo(streets_df, streets_buffered_df, dir_path, file_name):
     df.to_csv(f"{dir_path}\\with_streets_{file_name}")
     # plot_with_street_geo_data(df)
 
+def add_columns_geo_poi(streets_df, streets_buffered_df, dir_path, file_name):
+    print(f"Starting to proccess {file_name}")
+    df = gpd.read_file(f"{dir_path}\\{file_name}")
+    global non_intersect_counter
+    non_intersect_counter = 0
+
+    def get_closest_street(sample):
+        sample_location = sample["geometry"]
+        df_streets_filtered = streets_df[streets_buffered_df.intersects(sample_location)]
+        if df_streets_filtered.shape[0] == 0:
+            street_index = streets_df.distance(sample["geometry"]).argmin()
+            global non_intersect_counter
+            non_intersect_counter += 1
+        else:
+            inter = df_streets_filtered.intersection(sample_location)
+            uni = df_streets_filtered.union(sample_location)
+            street_filtered_index = (inter.area / uni.area).argmax()
+            street_index = df_streets_filtered.iloc[street_filtered_index]["index"]
+        return streets_df.iloc[street_index]["geometry"], street_index
+
+    df[['STREET', 'ST_INDEX']] = df.apply(get_closest_street, axis=1, result_type='expand')
+    print(f"data: {file_name} had {non_intersect_counter}"
+          f" number of items with no intersection with a street.")
+    print(f"data have {df.shape[0]}"
+          f" rows and {df['ST_INDEX'].isna().sum()} st_index null values and "
+          f"{df['STREET'].isna().sum()} street null values")
+    df.to_csv(f"{dir_path}\\with_streets_{file_name}")
+    # plot_with_street_geo_data(df)
+
 
 def plot_with_street_non_geo(df):
     longitude, latitude = get_long_lat_names(df)
@@ -128,6 +157,7 @@ def main(dir_path):
     for file_name in GEO_FILES_NAMES_TO_EXECUTE:
         if IS_GEO_FILES:
             add_columns_geo(streets_df, streets_buffered_df, dir_path, file_name)
+            add_columns_geo_poi(streets_df, streets_buffered_df, dir_path, file_name)
         if IS_NON_GEO_FILES:
             add_columns_non_geo(streets_df, streets_buffered_df, dir_path, file_name)
 
--- pre-process/clean_and_concat.py
+++ pre-process/clean_and_concat.py
@@ -3,21 +3,27 @@ import pandas as pd
 import geopandas as gpd
 import folium
 from shapely.geometry import Point
+import matplotlib.pyplot as plt
 import warnings
-
 from utils import ours_read_csv
+
+SOURCE_FOR_COLLISIONS_TYPES_VTL = "https://ypdcrime.com/vt/vt_guide.php"
+SOURCE_FOR_COLLISIONS_TYPES_NYC = "https://codelibrary.amlegal.com/codes/newyorkcity/latest/NYCrules/0-0-0" \
+                                  "-63614"
 
 warnings.filterwarnings("ignore")
 
 DIR_PATH_TOM = "C:\\Users\\ttomn\\OneDrive\\Desktop\\data-science-transportation-research"
 DIR_PATH_MAX = "C:\\Users\\maxba\\Desktop\\Study\\Third Year\\Final Project\\data-science-transportation-research"
 
-SUMMONS_FILE = "with_streets_not_na_NYPD_B_Summons__Year_to_Date_.csv"
+POI_FILE = "with_streets_Points Of Interest.csv"
+SUMMONS_FILE = "united_summons.csv"
+CURR_SUMMONS_FILE = "with_streets_not_na_NYPD_B_Summons__Year_to_Date_.csv"
 OLD_SUMMONS_FILE = "with_streets_not_na_NYPD_B_Summons__Historic_.csv"
 COLLISIONS_FILE = "with_streets_not_na_Motor_Vehicle_Collisions_-_Crashes.csv"
 FILES_NAMES_TO_EXECUTE = [SUMMONS_FILE, OLD_SUMMONS_FILE, COLLISIONS_FILE]
-COLS_TO_TAKE_FROM_SUMMONS = ["VIOLATION_DATE", "VIOLATION_TIME", "VIOLATION_CODE", "VEH_CATEGORY",
-                             "STREET", "ST_INDEX"]
+COLS_TO_TAKE_FROM_SUMMONS = ["VIOLATION_DATE", "VIOLATION_TIME", "CHG_LAW_CD", "VIOLATION_CODE",
+                             "VEH_CATEGORY", "STREET", "ST_INDEX"]
 
 COLS_TO_TAKE_FROM_COLLISIONS = ['CRASH DATE', 'CRASH TIME', 'NUMBER OF PERSONS INJURED',
                                 'NUMBER OF PERSONS KILLED', 'NUMBER OF PEDESTRIANS INJURED',
@@ -26,11 +32,31 @@ COLS_TO_TAKE_FROM_COLLISIONS = ['CRASH DATE', 'CRASH TIME', 'NUMBER OF PERSONS I
                                 'NUMBER OF MOTORIST KILLED', 'CONTRIBUTING FACTOR VEHICLE 1',
                                 'CONTRIBUTING FACTOR VEHICLE 2', 'CONTRIBUTING FACTOR VEHICLE 3',
                                 'CONTRIBUTING FACTOR VEHICLE 4', 'CONTRIBUTING FACTOR VEHICLE 5',
-                                'COLLISION_ID', 'VEHICLE TYPE CODE 1', 'VEHICLE TYPE CODE 2',
-                                'VEHICLE TYPE CODE 3', 'VEHICLE TYPE CODE 4', 'VEHICLE TYPE CODE 5',
+                                'VEHICLE TYPE CODE 1', 'VEHICLE TYPE CODE 2', 'VEHICLE TYPE CODE 3',
+                                'VEHICLE TYPE CODE 4', 'VEHICLE TYPE CODE 5',
                                 'STREET', 'ST_INDEX']
+
+COLS_TO_TAKE_FROM_POI = ["created", "facility_t", 'STREET', 'ST_INDEX']
+
 PARAMS_FOR_PROCESS = {
-    COLLISIONS_FILE: [COLS_TO_TAKE_FROM_COLLISIONS, 'CRASH DATE', 'CRASH TIME']
+    COLLISIONS_FILE: [COLS_TO_TAKE_FROM_COLLISIONS, 'CRASH DATE', 'CRASH TIME'],
+    SUMMONS_FILE: [COLS_TO_TAKE_FROM_SUMMONS, "VIOLATION_DATE", "VIOLATION_TIME"],
+    POI_FILE: [COLS_TO_TAKE_FROM_POI, "created", None]
+}
+POI_MAPPING = {
+    1: "Residential",
+    2: "Education",
+    3: "Cultural",
+    4: "Recreational",
+    5: "Social Services",
+    6: "Transportation",
+    7: "Commercial",
+    8: "Government",
+    9: "Religious Institution",
+    10: "Health Services",
+    11: "Public Safety",
+    12: "Water",
+    13: "Miscellaneous"
 }
 
 weekday_mapping = {
@@ -45,12 +71,12 @@ weekday_mapping = {
 
 
 def unit_summons(dir_path):
-    summons_df = ours_read_csv(f"{dir_path}\\{SUMMONS_FILE}")
+    summons_df = ours_read_csv(f"{dir_path}\\{CURR_SUMMONS_FILE}")
     old_summons_df = ours_read_csv(f"{DIR_PATH_TOM}\\{OLD_SUMMONS_FILE}")
     summons_df = create_summons_cols(summons_df)
     old_summons_df = create_summons_cols(old_summons_df)
     all_summons = pd.concat([summons_df, old_summons_df], axis=0)
-    all_summons.to_csv(f"{dir_path}\\united_summons.csv")
+    all_summons.to_csv(f"{dir_path}\\{SUMMONS_FILE}")
 
 
 def create_summons_cols(df):
@@ -62,49 +88,85 @@ def create_summons_cols(df):
 def aggregate_dfs(dir_path, files_names, cols_to_aggregate):
     for file_name in files_names:
         print(f"Start execute file {file_name}:")
-        df = ours_read_csv(f"{DIR_PATH_TOM}\\{file_name}")
+        df = ours_read_csv(f"{dir_path}\\{file_name}")
         df_params = PARAMS_FOR_PROCESS[file_name]
         df = df[df_params[0]]
         df = get_timing_cols(df, df_params[1], df_params[2])
-        df.to_csv(f"{dir_path}\\timed_{file_name}.csv")
-        df = agg_by_time(df, cols_to_aggregate)
-        df.to_csv(f"{dir_path}\\agg_{file_name}.csv")
+        df.to_csv(f"{dir_path}\\timed_{file_name}")
+        # df = agg_by_time_and_loc(df, cols_to_aggregate)
+        # df.to_csv(f"{dir_path}\\agg_{file_name}")
         print(f"\n\n\n############################################")
         print(f"Finished execute file {file_name}")
         print(f"############################################\n\n\n")
 
 
-def get_timing_cols(df, date_col_name, time_col_name):
+def get_timing_cols(df, date_col_name, time_col_name=None):
     date = pd.DatetimeIndex(df[date_col_name])
-    time = pd.DatetimeIndex(df[time_col_name])
     df['MONTH'] = date.month
     df['YEAR'] = date.year
     df['DAY'] = date.day
     df['DAY_OF_WEEK'] = date.day_name().map(weekday_mapping)
     df['WEEK_NUMBER'] = date.week
-    df['HOUR'] = time.hour
-    df['MINUTE'] = time.minute
-    df = df.drop(columns=[date_col_name, time_col_name])
+    if time_col_name is not None:
+        time = pd.DatetimeIndex(df[time_col_name])
+        df['HOUR'] = time.hour
+        df['MINUTE'] = time.minute
+        df = df.drop(columns=[date_col_name, time_col_name])
+    else:
+        df = df.drop(columns=[date_col_name])
     return df
 
 
-def agg_by_time(df, cols_to_aggregate):
+def agg_by_time_and_loc(df, cols_to_aggregate):
     df["AMOUNT"] = 0
     new_col_name = "AMOUNT"
     for col in cols_to_aggregate:
         new_col_name += ("_" + col)
     df[new_col_name] = 0
     df[new_col_name] = df.groupby(cols_to_aggregate).AMOUNT.transform('count')
-    df = df.drop(columns=["AMOUNT"])
+    df = df[[new_col_name, "STREET"] + cols_to_aggregate]
     return df
 
 
-# def concat_dfs
+def remove_zero_lat_long_from_colli(dir_path, file_name):
+    df = ours_read_csv(f"{dir_path}\\{file_name}")
+    filtered_df = df[
+        (df["LATITUDE"] > 40) & (df["LATITUDE"] < 42) & (df["LONGITUDE"] > -75) & (df["LONGITUDE"] < -73)]
+    df_params = PARAMS_FOR_PROCESS[file_name]
+    filtered_df = filtered_df[df_params[0]]
+    filtered_df = get_timing_cols(filtered_df, df_params[1], df_params[2])
+    filtered_df.to_csv(f"{dir_path}\\timed_with_streets_not_na_Motor_Vehicle_Collisions_-_Crashes.csv")
+
+
+def bin_summons(dir_path, file_name):
+    df = ours_read_csv(f"{dir_path}\\{file_name}")
+    vtl_filter = df["CHG_LAW_CD"] == "VTL"
+    df["VTL_NUMBERS"] = 0
+    vtl_numbers = df.loc[vtl_filter, "VIOLATION_CODE"].str.extract(r'^(\d+)', expand=False)
+    three_digits_filter = (~ vtl_numbers.str.startswith('1')) & (~ vtl_numbers.str.startswith('2'))
+    vtl_numbers[three_digits_filter] = vtl_numbers[three_digits_filter].astype(str).str.slice(stop=3)
+    df.loc[vtl_filter, "VTL_NUMBERS"] = vtl_numbers
+    return df
+
+
+def ohe_poi(dir_path, file_name):
+    df = ours_read_csv(f"{dir_path}\\{file_name}")
+    one_hot_df = pd.get_dummies(df["facility_t"])
+    one_hot_df.columns = POI_MAPPING.values()
+    df = pd.concat([df, one_hot_df], axis=1)
+    df = df.drop(columns="facility_t")
+    df.to_csv(f"{dir_path}\\ohe_{file_name}")
+    return df
+
 
 def main(dir_path):
-    # unit_summons(dir_path)
-    aggregate_dfs(dir_path, files_names=["with_streets_not_na_Motor_Vehicle_Collisions_-_Crashes.csv"],
-                  cols_to_aggregate=["YEAR", "MONTH"])
+    df_streets = ours_read_csv(f"{dir_path}\\Centerline.csv")
+    df = ours_read_csv(f"{dir_path}\\agg_timed_with_streets_not_na_Motor_Vehicle_Collisions_-_Crashes.csv")
+    print("hi")
+    # ohe_poi(dir_path, "timed_with_streets_Points Of Interest.csv")
+    # aggregate_dfs(dir_path, files_names=[POI_FILE],
+    #               cols_to_aggregate=["YEAR", "MONTH", "ST_INDEX"])
+    # bin_summons(dir_path, SUMMONS_FILE)
 
 
 if __name__ == '__main__':
